dad03abc94886175f4749f5b3a415c659a9be4a4
Daniel Laframboise <daniellaframboise@Daniel-Laframboises-M2-Pro.local>
2024-11-05 21:40:25 -0500

    Changes for mutli tenant public file support

--- routes/web.php
+++ routes/web.php
@@ -7,54 +7,34 @@ use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 // handle pagebuilder asset requests
-Route::any( config('pagebuilder.general.assets_url') . '{any}', function() {
+Route::any(config('pagebuilder.general.assets_url') . '{any}', function () {
     $builder = new LaravelPageBuilder(config('pagebuilder'));
     $builder->handlePageBuilderAssetRequest();
-
 })->where('any', '.*');
 
 
-// handle requests to retrieve uploaded file
-Route::middleware([
-    'web',
-    InitializeTenancyByDomain::class,
-    PreventAccessFromCentralDomains::class,
-])->group(function () {
-    Route::any( config('pagebuilder.general.uploads_url') . '{any}', function() {
-        $builder = new LaravelPageBuilder(config('pagebuilder'));
-        logger('handling uploaded file request');
-        // logger('tenant: ' . tenant()->getTenantKey());
-        logger("storage_path in web.php: " . storage_path());
-        $builder->handleUploadedFileRequest();
-    })->where('any', '.*');
-});
-
-
+Route::any(config('pagebuilder.general.uploads_url') . '{any}', function () {
+    $builder = new LaravelPageBuilder(config('pagebuilder'));
+    $builder->handleUploadedFileRequest();
+})->where('any', '.*');
 
 if (config('pagebuilder.website_manager.use_website_manager')) {
-
     // handle all website manager requests
-    Route::any( config('pagebuilder.website_manager.url') . '{any}', function() {
-
-        $builder = new LaravelPageBuilder(config('pagebuilder'));
-        $builder->handleRequest();
-
-    })->where('any', '.*');
-
+    Route::middleware([InitializeTenancyByDomain::class])->group(function () {
+        Route::any(config('pagebuilder.website_manager.url') . '{any}', function () {
+            $builder = new LaravelPageBuilder(config('pagebuilder'));
+            $builder->handleRequest();
+        })->where('any', '.*');
+    });
 }
 
-
 if (config('pagebuilder.router.use_router')) {
-
-    // pass all remaining requests to the LaravelPageBuilder router
-    Route::any( '/{any}', function() {
+    Route::any('/{any}', function () {
         $builder = new LaravelPageBuilder(config('pagebuilder'));
         $hasPageReturned = $builder->handlePublicRequest();
 
-        if (request()->path() === '/' && ! $hasPageReturned) {
+        if (request()->path() === '/' && !$hasPageReturned) {
             $builder->getWebsiteManager()->renderWelcomePage();
         }
-
     })->where('any', '.*');
-
 }
